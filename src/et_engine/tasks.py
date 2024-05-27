17d2b98d067409fe92af4183374a78eec962c8cf
Alex Miltenberger <alex.miltenberger@riotinto.com>
2024-05-27 11:07:42 -0700

    added task waiting feature

--- src/et_engine/tasks.py
+++ src/et_engine/tasks.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import os
+import time
+
 
 API_ENDPOINT = "https://t2pfsy11r1.execute-api.us-east-2.amazonaws.com/prod/"
 
@@ -22,3 +24,12 @@ class Task:
             return task_status
         else:
             raise Exception('error fetching status')
+        
+    def wait(self, frequency=60, max_tries=100):
+        for i in range(max_tries):
+            status = self.status()
+            print(status)
+            if status == "STOPPED":
+                print("task finished")
+                break
+            time.sleep(frequency)
