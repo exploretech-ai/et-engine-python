6c63841eeb2d96587620ff5aec3161fb402118ab
Alex Miltenberger <alex.miltenberger@riotinto.com>
2024-05-30 16:52:10 -0700

    fixed event listener bug on

--- et-engine/webapp/src/components/FilesDragAndDrop.jsx
+++ et-engine/webapp/src/components/FilesDragAndDrop.jsx
@@ -70,15 +70,14 @@ function FilesDragAndDrop({activeVFS, idToken, children}) {
         drop.current.addEventListener('dragover', handleDragOver)
         drop.current.addEventListener('drop', handleDrop)
         drop.current.addEventListener('dragenter', handleDragEnter)
-        drop.current.addEventListener('dragleave', handleDragLeave)
-
-        return () => {
-            drop.current.removeEventListener('dragover', handleDragOver)
-            drop.current.removeEventListener('drop', handleDrop)
-            drop.current.removeEventListener('dragenter', handleDragEnter)
-            drop.current.removeEventListener('dragleave', handleDragLeave)
-        }
+        drop.current.addEventListener('dragleave', handleDragLeave)        
       }
+      return () => {
+        drop.current.removeEventListener('dragover', handleDragOver)
+        drop.current.removeEventListener('drop', handleDrop)
+        drop.current.removeEventListener('dragenter', handleDragEnter)
+        drop.current.removeEventListener('dragleave', handleDragLeave)
+    }
   }, [activeVFS, idToken])
 
   const handleDragOver = (e) => {
